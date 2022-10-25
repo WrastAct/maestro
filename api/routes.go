3b4a8fcd5338a5cda7e862045441e3fb2fa8edb8
Wrast <donec2509@gmail.com>
2022-10-25 20:43:23 +0300

    Added most needed basic implementations

--- api/routes.go
+++ api/routes.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"expvar"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	//TODO: add handlers
+
+	// router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.requirePermission("admin", app.healthcheckHandler))
+	// router.HandlerFunc(http.MethodGet, "/v1/rooms", app.requireAuthenticatedUser(app.listRoomHandler))
+	// router.HandlerFunc(http.MethodPost, "/v1/rooms", app.requirePermission("user", app.createRoomHandler))
+	// router.HandlerFunc(http.MethodGet, "/v1/rooms/:id", app.requirePermission("user", app.showRoomHandler))
+	// router.HandlerFunc(http.MethodPatch, "/v1/rooms/:id", app.requirePermission("user", app.updateRoomHandler))
+	// router.HandlerFunc(http.MethodDelete, "/v1/rooms/:id", app.requirePermission("user", app.deleteRoomHandler))
+
+	// router.HandlerFunc(http.MethodGet, "/v1/furniture", app.listFurnitureHandler)
+	// router.HandlerFunc(http.MethodPost, "/v1/furniture", app.createFurnitureHandler)
+	// router.HandlerFunc(http.MethodGet, "/v1/furniture/:id", app.showFurnitureHandler)
+	// router.HandlerFunc(http.MethodPatch, "/v1/furniture/:id", app.updateFurnitureHandler)
+	// router.HandlerFunc(http.MethodDelete, "/v1/furniture/:id", app.deleteFurnitureHandler)
+
+	// router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	// router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+
+	// router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
+
+	//todo add authentication e.g. app.authenticate
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(router))))
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module github.com/WrastAct/maestro
 go 1.19
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
