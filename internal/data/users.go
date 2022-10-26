0ff51d727532823ce3652863bf2a43c055a14996
Wrast <donec2509@gmail.com>
2022-10-26 19:01:42 +0300

    Added users model

---
+++ internal/data/users.go
@@ -0,0 +1,237 @@
+package data
+
+import (
+	"context"
+	"crypto/sha256"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/WrastAct/maestro/internal/validator"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+var AnonymousUser = &User{}
+
+type User struct {
+	ID          int64     `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Nationality string    `json:"nationality"`
+	Birthday    string    `json:"birthday"`
+	Email       string    `json:"email"`
+	Password    password  `json:"-"`
+	Activated   bool      `json:"activated"`
+	VerifiedPro bool      `json:"verified_pro"`
+	Version     int       `json:"-"`
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 20, "password", "must not be more than 20 bytes long")
+}
+
+func ValidateDate(v *validator.Validator, date string) {
+	_, err := time.Parse("2006-01-02", date)
+	v.Check(err == nil, "date", "must be valid")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 100, "name", "must not be more than 100 bytes long")
+	v.Check(len(user.Nationality) <= 32, "nationality", "must not be more than 32 bytes long")
+	ValidateEmail(v, user.Email)
+	ValidateDate(v, user.Birthday)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m UserModel) Insert(user *User) error {
+	query := `
+		INSERT INTO users (users_name, users_description, nationality, birthday,
+			  				email, password_hash, activated, verified_pro)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		RETURNING users_id, created_at, version`
+
+	args := []interface{}{user.Name, user.Email, user.Password.hash, user.Activated}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m UserModel) GetByEmail(email string) (*User, error) {
+	query := `
+		SELECT users_id, created_at, users_name, users_description, nationality, 
+			   birthday, email, password_hash, activated, verified_pro, version
+		FROM users
+		WHERE email = $1`
+
+	var user User
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Description,
+		&user.Nationality,
+		&user.Birthday,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.VerifiedPro,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &user, nil
+}
+
+func (m UserModel) Update(user *User) error {
+	query := `
+		UPDATE users
+		SET users_name = $1, users_description = $2, nationality = $3, birthday = $4, email = $5, 
+			password_hash = $6, activated = $7, verified_pro = $8 version = version + 1
+		WHERE users_id = $9 AND version = $10
+		RETURNING version`
+
+	args := []interface{}{
+		user.Name,
+		user.Description,
+		user.Nationality,
+		user.Birthday,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+		user.VerifiedPro,
+		user.ID,
+		user.Version,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+
+	query := `
+		SELECT users.users_id, users.created_at, users.users_name, users.users_description, users.nationality,
+			   users.birthday, users.email, users.password_hash, users.activated, users.verified_pro, users.version
+		FROM users
+		INNER JOIN tokens
+		ON users.users_id = tokens.users_id
+		WHERE tokens.hash = $1
+		AND tokens.scope = $2
+		AND tokens.expiry > $3`
+
+	args := []interface{}{tokenHash[:], tokenScope, time.Now()}
+	var user User
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Description,
+		&user.Nationality,
+		&user.Birthday,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.VerifiedPro,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
